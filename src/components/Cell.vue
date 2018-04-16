03503b64f29fca553670dede1dc2bc1494312223
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-04-16 22:06:25 +0200

    First step to display a level

---
+++ src/components/Cell.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cell">
+      <img v-bind:src="cellImage" alt="toto" />
+  </div>
+</template>
+
+<script>
+const Tiles = {
+  WALL: require('../assets/sprites/blocks/block_08.png'),
+  GROUND: require('../assets/sprites/ground/ground_00.png'),
+  PLAYER: require('../assets/sprites/player/player_02.png'),
+  BOX: require('../assets/sprites/crates/crate_02.png'),
+  TARGET: require('../assets/sprites/environments/environment_02.png')
+}
+
+export default {
+  name: 'Cell',
+
+  props: ['type'],
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  computed: {
+    cellImage: function () {
+      if (this.type === '#') {
+        return Tiles.WALL
+      } else if (this.type === ' ') {
+        return Tiles.GROUND
+      } else if (this.type === '@') {
+        return Tiles.PLAYER
+      } else if (this.type === '$') {
+        return Tiles.BOX
+      } else if (this.type === '.') {
+        return Tiles.TARGET
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cell {
+  width: 64px;
+  height: 64px;
+}
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="grid">
+    <div v-for="row in formattedMap" class="grid-row">
+      <div v-for="cell in row">
+        <Cell v-bind:type="cell"></Cell>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Cell from './Cell'
+
+const levelOne = [
+  [' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', '#', '$', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', '#', '#', '#', ' ', ' ', '$', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', '#', ' ', ' ', '$', ' ', '$', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  ['#', '#', '#', ' ', '#', ' ', '#', '#', ' ', '#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#'],
+  ['#', ' ', ' ', ' ', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', ' ', '.', '.', '#'],
+  ['#', ' ', '$', ' ', ' ', '$', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', '.', '#'],
+  ['#', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', '#', '@', '#', '#', ' ', ' ', '.', '.', '#'],
+  [' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+  [' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+]
+
+export default {
+  name: 'Grid',
+
+  components: {
+    Cell
+  },
+
+  data () {
+    return {
+      levelOne
+    }
+  },
+
+  computed: {
+
+    formattedMap: function () {
+      const translatedMap = []
+
+      for (let i = 0; i < levelOne[0].length; i++) {
+        const row = []
+        for (let nbOfRows = 0; nbOfRows < levelOne.length; nbOfRows++) {
+          row.push(levelOne[nbOfRows][i])
+        }
+
+        translatedMap.push(row)
+      }
+
+      return translatedMap
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.grid-row {
+  display: inline-block;
+}
+
+</style>
